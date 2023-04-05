ac3ff081f1531d2bc6c0cbe2f8ec069b1aa4c2de
santino.fuentes <santino.fuentes@debian-8-amd64.fi.uncoma.edu.ar>
2023-04-05 12:19:42 -0300

    TDA

---
+++ 03-TP3_TDA/PrimerTDA/testAsociado.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import Asociado;
+public class testAsociado
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\t- Banco Pichincha -\n");
+	}
+}
