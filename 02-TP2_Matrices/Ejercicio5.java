c506e680c8d8e6d4f1fba18bd27bf8e2bf24b541
santino.fuentes <santino.fuentes@debian-8-amd64.fi.uncoma.edu.ar>
2023-04-10 11:53:22 -0300

    new

--- 02-TP2_Matrices/Ejercicio5.java
+++ 02-TP2_Matrices/Ejercicio5.java
@@ -2,27 +2,40 @@ public class Ejercicio5
 {
 	public static void comprobarMagia(double[][] cuadrado)
 	{
-		boolean esMagico;
+		boolean esMagico = false;
+		int m = 0;
 		int DIMENSION = cuadrado.length;
 		double sumaFilas[] = new double[DIMENSION];
 		double sumaColumnas[] = new double[DIMENSION];
+		double sumaDiagPpal[] = new double[DIMENSION];
+		double sumaDiagSecu[] = new double[DIMENSION];
 		for (int i = 0; i < DIMENSION; i++) {
 			for (int j = 0; j < DIMENSION; j++) {
 				sumaFilas[i] += cuadrado[i][j];
 				sumaColumnas[j] += cuadrado[i][j];
+				//k diagPpal
+				//l diagSecu
 			}
 		}
+		while (m < DIMENSION && !esMagico) {
+			// Comparar cada suma con la anterior
+			if (igual != sumaFilas[m]) {
+				esMagico = true;
+			}
+			m++;
+		}
+		return esMagico;
+		// Test
 		for (int k = 0; k < DIMENSION; k++) {
 			System.out.print("Suma fila " + k + ":" + sumaFilas[k]);
 			System.out.print("\n");
 			System.out.print("Suma columna " + k + ":" + sumaColumnas[k]);
 			System.out.print("\n------------------\n");
 		}
-		//return esMagico;
 	}
 	public static void main(String[] args)
 	{
-		double testCuadrado[][] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+		double testCuadrado[][] = {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}};
 		comprobarMagia(testCuadrado);
 	}
 }
