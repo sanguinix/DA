06c9218095ff926784627a9233a35538f5d9802e
sanguinix <santinofuentes24.sf@gmail.com>
2023-05-13 00:20:53 -0300

    recursión

---
+++ 07-TP4_Recursividad/Ejercicio4.java
@@ -0,0 +1,29 @@
+public class Ejercicio4
+{
+	/**
+	 * Recorre todo el número para ver si cada una de sus componentes
+	 * son iguales
+	 */
+	public static boolean iguales(int num)
+	{
+		boolean igual = false;
+		if (num % 10 == num) {
+			// Recién en el último número se verifica si es cierto
+			igual = true;
+		} else if (((num / 10) % 10) != (num % 10)) {
+			// Si el anteúltimo y el último número son diferentes
+			igual = false;
+		} else {
+			// Si aún son iguales, entra al paso recursivo :)
+			igual = iguales(num / 10);
+		}
+		return (igual);
+	}
+	/**
+	 * Test con un número arbitrario
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("" + iguales(1211));
+	}
+}
